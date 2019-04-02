071062c5ed6dec9230546e0fcd82d9e1ab9c7d81
Andrew Tulloch <andrew@tullo.ch>
2019-04-01 20:14:01 -0700

    Softmax working

--- src/contrib/wavernn/wavernn.cc
+++ src/contrib/wavernn/wavernn.cc
@@ -8,7 +8,7 @@
 #include <dlpack/dlpack.h>
 #include <algorithm>
 #include <vector>
-
+#include <random>
 #include "../../runtime/graph/graph_runtime.h"
 
 namespace tvm {
@@ -16,15 +16,80 @@ namespace contrib {
 
 using namespace runtime;
 
+
+// TODO: seed consistently.
+static std::unique_ptr<std::mt19937> gen; //Standard mersenne_twister_engine seeded with rd()
+
+TVM_REGISTER_GLOBAL("tvm.contrib.wavernn.set_seed")
+.set_body([](TVMArgs args, TVMRetValue *ret) {
+  int seed = args[0];
+  gen.reset(new std::mt19937(seed));
+});
+
+TVM_REGISTER_GLOBAL("tvm.contrib.wavernn.random_uniform")
+.set_body([](TVMArgs args, TVMRetValue *ret) {
+  std::uniform_real_distribution<float> dis;
+  *ret = dis(*gen);
+});
+
 TVM_REGISTER_GLOBAL("tvm.contrib.wavernn.frame")
 .set_body([](TVMArgs args, TVMRetValue *ret) {
-  DLTensor *a1 = args[0];
-  DLTensor *a2 = args[1];
-  DLTensor *m = args[2];
-  DLTensor *x_0 = args[3];
-  DLTensor *h1_0 = args[4];
-  Module sample_module = args[5];
-  auto* graph_runtime = static_cast<GraphRuntime*>(sample_module.operator->());
+  DLTensor* I_residual = args[0];
+  DLTensor* fc1_residual = args[1];
+  DLTensor* x_0 = args[2];
+  DLTensor* h1_0 = args[3];
+  Module sample_module = args[4];
+  DLTensor* outs = args[5];
+  DLTensor* h1_out = args[6];
+  DLTensor* x_proba = args[7];
+  DLTensor* I_residual_t = args[8];
+  DLTensor* fc1_residual_t = args[9];
+  auto* gr = static_cast<GraphRuntime*>(sample_module.operator->());
+
+  const size_t T = I_residual->shape[0];
+
+  DLTensor* x = x_0;
+  DLTensor* h1 = h1_0;
+
+  auto sample_proba = [&](const float* p) -> float {
+    std::uniform_real_distribution<float> dis;
+    const float rand = dis(*gen);
+    size_t rand_sample = 0;
+    float prob_sum = p[0];
+    while (prob_sum < rand) {
+      rand_sample += 1;
+      prob_sum += p[rand_sample];
+    }
+    return static_cast<float>(rand_sample) / 256;
+  };
+
+  for (size_t t = 0; t < T; ++t) {
+    gr->SetInput(gr->GetInputIndex("x"), x);
+    gr->SetInput(gr->GetInputIndex("h1"), h1);
+    std::copy_n(static_cast<float*>(I_residual->data) + t * I_residual->shape[1],
+                I_residual->shape[1], static_cast<float*>(I_residual_t->data));
+    gr->SetInput(gr->GetInputIndex("I_residual"), const_cast<DLTensor*>(I_residual_t));
+    std::copy_n(static_cast<float*>(fc1_residual->data) + t * fc1_residual->shape[1],
+                fc1_residual->shape[1], static_cast<float*>(fc1_residual_t->data));
+
+    gr->SetInput(gr->GetInputIndex("fc1_residual"), const_cast<DLTensor*>(fc1_residual_t));
+    gr->Run();
+
+    // Update h1
+    gr->CopyOutputTo(1, h1);
+
+
+    // Compute and update new sampled x values
+    gr->CopyOutputTo(0, x_proba);
+
+
+    const auto x_t = sample_proba(static_cast<const float*>(x_proba->data));
+    static_cast<float*>(outs->data)[t] = x_t;
+    static_cast<float*>(x->data)[0] = x_t;
+  }
+
+  // Copy out final hidden state.
+  std::copy_n(static_cast<const float*>(h1->data), h1->shape[1], static_cast<float*>(h1_out->data));
 
 });
 
--- wavernn/pytorch_frame.py
+++ wavernn/pytorch_frame.py
@@ -27,108 +27,130 @@ rnn1 = nn.GRUCell(rnn_dims, rnn_dims)
 fc1 = nn.Linear(rnn_dims + aux_dims, fc_dims)
 fc2 = nn.Linear(fc_dims, n_classes)
 
+def tvm_random_seed(seed):
+    import tvm
+    tvm.get_global_func("tvm.contrib.wavernn.set_seed")(seed)
+
+def tvm_random_uniform():
+    import tvm
+    return tvm.get_global_func("tvm.contrib.wavernn.random_uniform")()
+
+
 def sample(x_prob):
     gumbel = -torch.log(-torch.log(torch.tensor(np.random.uniform(size=x_prob.shape).astype("float32"))))
     result = np.zeros((1, 1), dtype="float32")
     result[:] = np.argmax(x_prob - gumbel)
     return torch.tensor(result / n_classes)
 
+def sample_proba(x_prob):
+    rand_sample = 0
+    prob_sum = x_prob[0][0]
+    rand = tvm_random_uniform()
+
+    while prob_sum < rand:
+        rand_sample += 1
+        prob_sum += x_prob[0][rand_sample]
+
+    result = np.zeros((1, 1), dtype="float32")
+    result[:] = rand_sample
+    return torch.tensor(result / n_classes)
+
 def reference_frame(a1, a2, m, x_0, h1_0):
-    np.random.seed(10)
-    with torch.no_grad():
-        (x, h1) = (x_0, h1_0)
-        T = a1.shape[0]
-        outs = []
-        for t in range(T):
-            xconcat = torch.cat([x, m[0, t:t+1], a1[0, t:t+1]], dim=1)
-            xconcat_trns = I(xconcat)
-            h1 = rnn1(xconcat_trns, h1)
-            xres = xconcat_trns + h1
-            xres_concat = torch.cat([xres, a2[0, t:t+1]], dim=1)
-            x_fc = F.relu(fc1(xres_concat))
-            x_prob = fc2(x_fc)
-            x = sample(x_prob)
-            outs.append(x)
-        return outs, h1
+    tvm_random_seed(10)
+    (x, h1) = (x_0, h1_0)
+    T = a1.shape[1]
+    outs = []
+    for t in range(T):
+        xconcat = torch.cat([x, m[0, t:t+1], a1[0, t:t+1]], dim=1)
+        xconcat_trns = I(xconcat)
+        h1 = rnn1(xconcat_trns, h1)
+        xres = xconcat_trns + h1
+        xres_concat = torch.cat([xres, a2[0, t:t+1]], dim=1)
+        x_fc = F.relu(fc1(xres_concat))
+        x_prob = fc2(x_fc)
+        x_prob = torch.softmax(x_prob, dim=1)
+        x = sample_proba(x_prob)
+        outs.append(x)
+    return outs, h1
 
 
 def reference():
     return reference_frame(a1, a2, m, x_0, h1_0)
 
 def test_pytorch_reference():
-    outs_ref, h1_ref = reference()
-    outs_new, h1_new = reference_frame(a1, a2, m, x_0, h1_0)
-    np.testing.assert_allclose(outs_ref, outs_new)
-    np.testing.assert_allclose(h1_ref, h1_new)
+    with torch.no_grad():
+        outs_ref, h1_ref = reference()
+        outs_new, h1_new = reference_frame(a1, a2, m, x_0, h1_0)
+        np.testing.assert_allclose(outs_ref, outs_new)
+        np.testing.assert_allclose(h1_ref, h1_new)
 
 def factored_premul_frame(a1, a2, m, x_0, h1_0):
-    np.random.seed(10)
-    with torch.no_grad():
-        I_residual =  m[0] @ I.weight[:, 1:1 + feat_dims].transpose(1, 0) + a1[0] @ I.weight[:, 1 + feat_dims:].transpose(1, 0)
-        fc1_residual = a2[0] @ fc1.weight[:, rnn_dims:].transpose(1, 0)
-        I_weight = I.weight[:]
-        I_bias = I.bias[:]
+    tvm_random_seed(10)
+    I_residual =  m[0] @ I.weight[:, 1:1 + feat_dims].transpose(1, 0) + a1[0] @ I.weight[:, 1 + feat_dims:].transpose(1, 0)
+    fc1_residual = a2[0] @ fc1.weight[:, rnn_dims:].transpose(1, 0)
+    I_weight = I.weight[:]
+    I_bias = I.bias[:]
 
-        Ifactored = nn.Linear(1, rnn_dims)
-        Ifactored.weight[:, :] = I.weight[:, :1]
-        Ifactored.bias[:] = I.bias[:]
+    Ifactored = nn.Linear(1, rnn_dims)
+    Ifactored.weight[:, :] = I.weight[:, :1]
+    Ifactored.bias[:] = I.bias[:]
 
-        (x, h1) = (x_0, h1_0)
-        outs = []
-        T = a1.shape[0]
-        for t in range(T):
-            xconcat_trns = Ifactored(x) + I_residual[t:t+1]
+    (x, h1) = (x_0, h1_0)
+    outs = []
+    T = a1.shape[1]
+    for t in range(T):
+        xconcat_trns = Ifactored(x) + I_residual[t:t+1]
 
-            def gru_cell(cell, x, h):
-                xt = x @ cell.weight_ih.transpose(1, 0) + cell.bias_ih
-                ht = h @ cell.weight_hh.transpose(1, 0) + cell.bias_hh
-                assert xt.shape == (1, 3 * rnn_dims)
-                assert ht.shape == (1, 3 * rnn_dims)
+        def gru_cell(cell, x, h):
+            xt = x @ cell.weight_ih.transpose(1, 0) + cell.bias_ih
+            ht = h @ cell.weight_hh.transpose(1, 0) + cell.bias_hh
+            assert xt.shape == (1, 3 * rnn_dims)
+            assert ht.shape == (1, 3 * rnn_dims)
 
-                reset_gate = torch.sigmoid(xt[:, 0:rnn_dims] + ht[:, 0:rnn_dims])
-                input_gate = torch.sigmoid(xt[:, rnn_dims:2 * rnn_dims] + ht[:, rnn_dims:2 * rnn_dims])
-                new_gate = torch.tanh(xt[:, 2 * rnn_dims:3 * rnn_dims] + reset_gate * ht[:, 2 * rnn_dims:3 * rnn_dims])
-                return new_gate + input_gate * (h - new_gate)
+            reset_gate = torch.sigmoid(xt[:, 0:rnn_dims] + ht[:, 0:rnn_dims])
+            input_gate = torch.sigmoid(xt[:, rnn_dims:2 * rnn_dims] + ht[:, rnn_dims:2 * rnn_dims])
+            new_gate = torch.tanh(xt[:, 2 * rnn_dims:3 * rnn_dims] + reset_gate * ht[:, 2 * rnn_dims:3 * rnn_dims])
+            return new_gate + input_gate * (h - new_gate)
 
-            h1 = gru_cell(rnn1, xconcat_trns, h1)
+        h1 = gru_cell(rnn1, xconcat_trns, h1)
 
-            xres = xconcat_trns + h1
-
-            fc1factored = nn.Linear(rnn_dims, fc_dims)
-            fc1factored.weight[:, :] = fc1.weight[:, :rnn_dims]
-            fc1factored.bias[:] = fc1.bias[:]
-
-            x_fc = F.relu(fc1factored(xres) + fc1_residual[t:t+1])
-            x_prob = fc2(x_fc)
-            x = sample(x_prob)
-            outs.append(x)
-        return outs, h1
-
-def build_wavernn_module():
-    from tvm import relay
-    import tvm
-
-    with torch.no_grad():
-        Ifactored = nn.Linear(1, rnn_dims)
-        Ifactored.weight[:, :] = I.weight[:, :1]
-        Ifactored.bias[:] = I.bias[:]
+        xres = xconcat_trns + h1
 
         fc1factored = nn.Linear(rnn_dims, fc_dims)
         fc1factored.weight[:, :] = fc1.weight[:, :rnn_dims]
         fc1factored.bias[:] = fc1.bias[:]
 
-        params = {
-            "I_W": tvm.ndarray.array(Ifactored.weight.detach().numpy()),
-            "I_B": tvm.ndarray.array(Ifactored.bias.detach().numpy()),
-            "rnn1_weight_ih": tvm.ndarray.array(rnn1.weight_ih.detach().numpy()),
-            "rnn1_weight_hh": tvm.ndarray.array(rnn1.weight_hh.detach().numpy()),
-            "rnn1_bias_ih": tvm.ndarray.array(rnn1.bias_ih.detach().numpy()),
-            "rnn1_bias_hh": tvm.ndarray.array(rnn1.bias_hh.detach().numpy()),
-            "fc1_W": tvm.ndarray.array(fc1factored.weight.detach().numpy()),
-            "fc1_B": tvm.ndarray.array(fc1factored.bias.detach().numpy()),
-            "fc2_W": tvm.ndarray.array(fc2.weight.detach().numpy()),
-            "fc2_B": tvm.ndarray.array(fc2.bias.detach().numpy()),
-        }
+        x_fc = F.relu(fc1factored(xres) + fc1_residual[t:t+1])
+        x_prob = fc2(x_fc)
+        x_prob = torch.softmax(x_prob, dim=1)
+        x = sample_proba(x_prob)
+        outs.append(x)
+    return outs, h1
+
+def build_wavernn_module():
+    from tvm import relay
+    import tvm
+
+    Ifactored = nn.Linear(1, rnn_dims)
+    Ifactored.weight[:, :] = I.weight[:, :1]
+    Ifactored.bias[:] = I.bias[:]
+
+    fc1factored = nn.Linear(rnn_dims, fc_dims)
+    fc1factored.weight[:, :] = fc1.weight[:, :rnn_dims]
+    fc1factored.bias[:] = fc1.bias[:]
+
+    params = {
+        "I_W": tvm.ndarray.array(Ifactored.weight.detach().numpy()),
+        "I_B": tvm.ndarray.array(Ifactored.bias.detach().numpy()),
+        "rnn1_weight_ih": tvm.ndarray.array(rnn1.weight_ih.detach().numpy()),
+        "rnn1_weight_hh": tvm.ndarray.array(rnn1.weight_hh.detach().numpy()),
+        "rnn1_bias_ih": tvm.ndarray.array(rnn1.bias_ih.detach().numpy()),
+        "rnn1_bias_hh": tvm.ndarray.array(rnn1.bias_hh.detach().numpy()),
+        "fc1_W": tvm.ndarray.array(fc1factored.weight.detach().numpy()),
+        "fc1_B": tvm.ndarray.array(fc1factored.bias.detach().numpy()),
+        "fc2_W": tvm.ndarray.array(fc2.weight.detach().numpy()),
+        "fc2_B": tvm.ndarray.array(fc2.bias.detach().numpy()),
+    }
 
     Rx = relay.var("x", shape=[1, 1], dtype="float32")
     Rh1 = relay.var("h1", shape=[1, rnn_dims], dtype="float32")
@@ -171,7 +193,7 @@ def build_wavernn_module():
     Rfc2_W = relay.var("fc2_W", shape=(n_classes, fc_dims), dtype="float32")
     Rfc2_B = relay.var("fc2_B", shape=(n_classes,), dtype="float32")
 
-    x_prob = dense(x_fc, Rfc2_W, Rfc2_B)
+    x_prob = relay.nn.softmax(dense(x_fc, Rfc2_W, Rfc2_B))
 
     outputs = relay.expr.Tuple([x_prob, h1])
     func = relay.Function(relay.ir_pass.free_vars(outputs), outputs)
@@ -184,10 +206,10 @@ def build_wavernn_module():
 
 def factored_relay_frame(a1, a2, m, x_0, h1_0):
     import tvm
-    np.random.seed(10)
+    tvm_random_seed(10)
     (x, h1) = (tvm.ndarray.array(x_0), tvm.ndarray.array(h1_0))
     outs = []
-    T = a1.shape[0]
+    T = a1.shape[1]
     module = build_wavernn_module()
 
     I_residual = m[0] @ I.weight[:, 1:1 + feat_dims].transpose(1, 0) + a1[0] @ I.weight[:, 1 + feat_dims:].transpose(1, 0)
@@ -204,18 +226,59 @@ def factored_relay_frame(a1, a2, m, x_0, h1_0):
         module.set_input(**inputs)
         module.run()
         (x_prob, h1) = module.get_output(0), module.get_output(1)
-        x = tvm.ndarray.array(sample(torch.tensor(x_prob.asnumpy())))
+        x = tvm.ndarray.array(sample_proba(torch.tensor(x_prob.asnumpy())))
         outs.append(x.asnumpy()[0][0])
     return outs, h1.asnumpy()
 
+def factored_relay_cpp_frame(a1, a2, m, x_0, h1_0):
+    import tvm
+    tvm_random_seed(10)
+    (x, h1) = (tvm.ndarray.array(x_0), tvm.ndarray.array(h1_0))
+    T = a1.shape[1]
+    module = build_wavernn_module()
+
+    I_residual = m[0] @ I.weight[:, 1:1 + feat_dims].transpose(1, 0) + a1[0] @ I.weight[:, 1 + feat_dims:].transpose(1, 0)
+    fc1_residual = a2[0] @ fc1.weight[:, rnn_dims:].transpose(1, 0)
+
+    frame_func = tvm.get_global_func("tvm.contrib.wavernn.frame")
+
+    outs = tvm.ndarray.array(np.random.randn(T).astype("float32"))
+    h1 = tvm.ndarray.array(np.random.randn(1, rnn_dims).astype("float32"))
+    frame_func(
+        # Inputs
+        tvm.ndarray.array(I_residual),
+        tvm.ndarray.array(fc1_residual),
+        tvm.ndarray.array(x_0),
+        tvm.ndarray.array(h1_0),
+        # Runtime module
+        module.module,
+        # Outputs
+        outs,
+        h1,
+        # Purely for temp-storage
+        tvm.ndarray.array(np.random.randn(1, n_classes).astype("float32")),
+        tvm.ndarray.array(np.random.randn(1, rnn_dims).astype("float32")),
+        tvm.ndarray.array(np.random.randn(1, fc_dims).astype("float32")),
+    )
+    return outs.asnumpy(), h1.asnumpy()
+
 def test_factored_premul_frame():
-    outs_ref, h1_ref = reference()
-    outs_new, h1_new = factored_premul_frame(a1, a2, m, x_0, h1_0)
-    np.testing.assert_allclose(outs_ref, outs_new, rtol=1e-4, atol=1e-4)
-    np.testing.assert_allclose(h1_ref, h1_new, rtol=1e-4, atol=1e-4)
+    with torch.no_grad():
+        outs_ref, h1_ref = reference()
+        outs_new, h1_new = factored_premul_frame(a1, a2, m, x_0, h1_0)
+        np.testing.assert_allclose(outs_ref, outs_new, rtol=1e-4, atol=1e-4)
+        np.testing.assert_allclose(h1_ref, h1_new, rtol=1e-4, atol=1e-4)
 
 def test_relay_frame():
-    outs_ref, h1_ref = reference()
-    outs_new, h1_new = factored_relay_frame(a1, a2, m, x_0, h1_0)
-    np.testing.assert_allclose(outs_ref, outs_new, rtol=1e-4, atol=1e-4)
-    np.testing.assert_allclose(h1_ref, h1_new, rtol=1e-4, atol=1e-4)
+    with torch.no_grad():
+        outs_ref, h1_ref = reference()
+        outs_new, h1_new = factored_relay_frame(a1, a2, m, x_0, h1_0)
+        np.testing.assert_allclose(outs_ref, outs_new, rtol=1e-4, atol=1e-4)
+        np.testing.assert_allclose(h1_ref, h1_new, rtol=1e-4, atol=1e-4)
+
+def test_relay_cpp_frame():
+    with torch.no_grad():
+        outs_ref, h1_ref = reference()
+        outs_new, h1_new = factored_relay_cpp_frame(a1, a2, m, x_0, h1_0)
+        np.testing.assert_allclose(outs_ref, outs_new, rtol=1e-4, atol=1e-4)
+        np.testing.assert_allclose(h1_ref, h1_new, rtol=1e-4, atol=1e-4)
